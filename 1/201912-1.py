80d51e4216cdee329ccc592efac3a60c2ff6bdf1
scarlett <liangchuanluo@gmail.com>
2021-09-04 10:32:27 +0800

    201912-1 报数

--- 1/201912-1.py
+++ 1/201912-1.py
@@ -1,3 +1,23 @@
 n = int(input())
 counts = [0, 0, 0, 0]
-for i in range(1, n + 1):
+i = 0
+count = 0
+while True:
+    i += 1
+    if i % 7 == 0 or '7' in str(i):
+        if i % 4 == 1:
+            counts[0] += 1
+        elif i % 4 == 2:
+            counts[1] += 1
+        elif i % 4 == 3:
+            counts[2] += 1
+        else:
+            counts[3] += 1
+    else:
+        count += 1
+        if count == n:
+            break
+print(counts[0])
+print(counts[1])
+print(counts[2])
+print(counts[3], end='')
